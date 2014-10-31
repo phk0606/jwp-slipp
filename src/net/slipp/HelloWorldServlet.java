736220639d787fbd70557e403c88fee5db837c0f
parkjaesung <javajigi@gmail.com>
2014-10-31 10:22:34 +0900

    Servlet 3.0 기반으로 서블릿 매핑함.

--- src/net/slipp/HelloWorldServlet.java
+++ src/net/slipp/HelloWorldServlet.java
@@ -3,10 +3,12 @@ package net.slipp;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name="HelloWorld", urlPatterns={"/helloworld", "/hello", "/hello/world"})
 public class HelloWorldServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
